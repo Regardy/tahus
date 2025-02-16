2822b5516d726753132ccae05a5fb0f76f76ceca
Rega <148520994+Regardy@users.noreply.github.com>
2025-02-16 14:56:43 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,10 +1,9 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
-const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
+const { createClient } = require("@supabase/supabase-js");
 
-const db = require("./config/database");
 const donationRoutes = require("./routes/donations");
 const webhookRoutes = require("./routes/webhook");
 
@@ -21,18 +20,3 @@ app.use(express.static("public"));
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
-
-// config/database.js
-const mongoose = require("mongoose");
-const MONGO_URI = process.env.MONGO_URI;
-
-mongoose.connect(MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
-
-mongoose.connection.on("connected", () => {
-  console.log("Connected to MongoDB");
-});
-
-module.exports = mongoose;
