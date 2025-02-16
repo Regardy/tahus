248b988f673da25ff9b282490a024ce645cec858
Rega <148520994+Regardy@users.noreply.github.com>
2025-02-16 15:18:24 +0700

    Update donations.js

--- routes/donations.js
+++ routes/donations.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const auth = require("../middleware/auth");
 const { getDonations } = require("../controllers/donationController");
 
+// Define route to get donation list, protected with authentication middleware
 router.get("/", auth, getDonations);
 
 module.exports = router;
