5c5a6103e76a7049b98cf35c60671949ee8cad29
Rega <148520994+Regardy@users.noreply.github.com>
2025-02-16 15:18:52 +0700

    Update webhook.js

--- routes/webhook.js
+++ routes/webhook.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const auth = require("../middleware/auth");
 const { createDonation } = require("../controllers/donationController");
 
+// Webhook route to handle incoming donations from Saweria
 router.post("/saweria", auth, createDonation);
 
 module.exports = router;
